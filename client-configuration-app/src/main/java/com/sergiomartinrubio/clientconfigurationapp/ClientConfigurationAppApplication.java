a3afedf0eaff85cbab84f7bbbf9912d1bf4e3af7
smartinrubio <smartinrubio@hotels.com>
2019-10-18 17:14:10 +0100

    add properties

--- client-configuration-app/src/main/java/com/sergiomartinrubio/clientconfigurationapp/ClientConfigurationAppApplication.java
+++ client-configuration-app/src/main/java/com/sergiomartinrubio/clientconfigurationapp/ClientConfigurationAppApplication.java
@@ -1,13 +1,27 @@
 package com.sergiomartinrubio.clientconfigurationapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@RequiredArgsConstructor
+@EnableConfigurationProperties(value = MySqlProperties.class)
 public class ClientConfigurationAppApplication {
+
+    private final MySqlProperties mySqlProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientConfigurationAppApplication.class, args);
+    }
+
+    @GetMapping
+    public String getProperty() {
+        return mySqlProperties.getUsername();
     }
 
 }
---
+++ client-configuration-app/src/main/java/com/sergiomartinrubio/clientconfigurationapp/MySqlProperties.java
@@ -0,0 +1,14 @@
+package com.sergiomartinrubio.clientconfigurationapp;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "mysql.properties")
+public class MySqlProperties {
+
+    private String uri;
+    private String username;
+    private String password;
+
+}
