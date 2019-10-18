8d88ec6be8bfbcbbc766d3b8a04c436bad267b51
smartinrubio <smartinrubio@hotels.com>
2019-10-18 23:46:51 +0100

    update configuration

--- client-configuration-app/src/main/java/com/sergiomartinrubio/clientconfigurationapp/ClientConfigurationAppApplication.java
+++ client-configuration-app/src/main/java/com/sergiomartinrubio/clientconfigurationapp/ClientConfigurationAppApplication.java
@@ -4,24 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
-@RequiredArgsConstructor
 @EnableConfigurationProperties(value = MySqlProperties.class)
 public class ClientConfigurationAppApplication {
-
-    private final MySqlProperties mySqlProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientConfigurationAppApplication.class, args);
     }
+}
+
+@RefreshScope
+@RestController
+@RequiredArgsConstructor
+class ClientConfigurationController {
+
+    private final MySqlProperties mySqlProperties;
 
     @GetMapping
     public String getProperty() {
         return mySqlProperties.getUsername();
     }
-
 }
